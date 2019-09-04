7a017bd9e7f506276e28a138a040f5b41c08b367
Nikola Cvetkovski <cvetkovskinjp@gmail.com>
2019-09-04 11:51:28 +0900

    change the way the API_KEY env is read

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,5 @@
 const pkg = require('./package')
 require('dotenv').config()
-const { API_KEY } = process.env
 
 const ampBoilerplate =
   '<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>'
@@ -96,7 +95,7 @@ module.exports = {
   ],
 
   env: {
-    apiKey: API_KEY
+    apiKey: process.env.API_KEY
   },
 
   server: {
