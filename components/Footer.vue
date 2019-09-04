41c33ad9d4732a20801b5d7464595e219f5fee2b
Nikola Cvetkovski <cvetkovskinjp@gmail.com>
2019-09-04 15:53:05 +0900

    adding credits in the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,23 @@
   <footer>
     <div class="footer">
       <div class="credits">
-        <span>Made by: Nikola Cvetkovski</span>
+        <span>Made by: <a href="https://github.com/cvetkovskin" target="_blank">Nikola Cvetkovski</a></span>
+        <span>
+          Banner background image by
+          <a
+            href="https://pixabay.com/users/BUMIPUTRA-731298/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2926963"
+            target="_blank"
+          >
+            BUMIPUTRA
+          </a>
+          from
+          <a
+            href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2926963"
+            target="_blank"
+          >
+            Pixabay
+          </a>
+        </span>
       </div>
     </div>
   </footer>
@@ -15,5 +31,15 @@
   display: flex;
   align-items: center;
   justify-content: flex-end;
+
+  .credits {
+    display: flex;
+    flex-flow: column;
+
+    a {
+      color: #030303;
+      font-weight: bold;
+    }
+  }
 }
 </style>
