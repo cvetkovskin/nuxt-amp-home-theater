ad79b2d5eb571196274de47398363183233c26ce
Nikola Cvetkovski <cvetkovskinjp@gmail.com>
2019-09-09 16:44:36 +0900

    adding the app.html file and making changes to the config consequently

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,6 @@ const ampBoilerplate =
   '<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>'
 
 const modifyHtml = html => {
-  html = html.replace(/<html/, '<html amp')
-
   // Add amp-custom tag to added CSS and join all the CSS into one <style-tag>
   let styleConcat = ''
   html = html.replace(/(<style\b[^<>]*>)([^<]*)(<\/style>)/gi, (_match, p1, p2) => {
@@ -18,7 +16,7 @@ const modifyHtml = html => {
 
   // Remove every script tag from generated HTML except the JSON type for the amp-state or the AMP templates
   html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, match => {
-    if (match.includes(`type="application/json"`) || match.includes(`template="amp-mustache"`)) {
+    if (match.includes(`type="application/json"`)) {
       return match.replace(/&quot;/g, '"')
     }
 
